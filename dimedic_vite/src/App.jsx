9203cf01866730e573f95ed14a4d6182558eabd5
Anna <michalowska.aneczka@gmail.com>
2023-04-03 19:46:49 +0200

    commit_interview

--- dimedic_vite/src/App.jsx
+++ dimedic_vite/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { create } from "zustand";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const useCarrotStore = create((set) => ({
+    carrots: 0,
+    increasePopulatioOrangen: () => set((state) => ({ carrots: state.carrots + 1 })),
+    removeAllCarrot: () => set({ carrot: 0 }),
+  }));
+
+  //function CarrotCounter() {
+  const carrots = useCarrotStore((state) => state.carrots);
+  //return <h1>{bears} around here ...</h1>;
+  //}
+
+  //function Controls() {
+  const increasePopulation = useCarrotStore(
+    (state) => state.increasePopulation
+  );
+  //return <button onClick={increasePopulation}>one up</button>;
+  //}
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={increasePopulationOrange}>one up orange</button>;
+      <h1>{carrots} Carrot orange</h1>;
+      <button onClick={increasePopulation}>one up violet</button>;
+      <h1>{carrots} Carrot violet</h1>;
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
